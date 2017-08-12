8b1616a87eda460bfcae4f771117977baff0954e
LBJ <LBJ>
2017-08-12 15:44:31 +0800

    20170812

--- app/src/main/java/tw/ming/app/helloworid/myviewtest/MainActivity.java
+++ app/src/main/java/tw/ming/app/helloworid/myviewtest/MainActivity.java
@@ -1,17 +1,62 @@
 package tw.ming.app.helloworid.myviewtest;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 public class MainActivity extends AppCompatActivity {
-
+    MyView myView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.gc();
-        Runtime.getRuntime().gc();
+        myView = (MyView)findViewById(R.id.myview);
+//        System.gc();
+//        Runtime.getRuntime().gc();
     }
+    public void clear(View view){
+        myView.clear();
+    }
+
+    public void color(View view){
+        ColorPickerDialogBuilder
+                .with(this)
+                .setTitle("Choose color")
+                .initialColor(myView.getColor())
+                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                .density(12)
+                .setOnColorSelectedListener(new OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int selectedColor) {
+                        Log.i("brad", "onColorSelected:" + selectedColor);
+                    }
+                })
+                .setPositiveButton("ok", new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                        Log.i("brad", "onClick:" + selectedColor);
+                        myView.setColor(selectedColor);
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .build()
+                .show();
+    }
+
 }
--- app/src/main/java/tw/ming/app/helloworid/myviewtest/MyView.java
+++ app/src/main/java/tw/ming/app/helloworid/myviewtest/MyView.java
@@ -6,41 +6,88 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
+
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.LinkedList;
 
 /**
  * Created by Mac on 2017/8/12.
  */
 
 public class MyView extends View{
-
+    private LinkedList<LinkedList<Point>> lines;
+    private Paint paint;
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         setBackgroundColor(Color.GREEN);
+        paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(4);
+
+
+        lines = new LinkedList<>();
 //        setOnClickListener(new OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
-//                Log.i("ming","onClick");
+//                Log.i("brad", "onClick");
 //            }
 //        });
     }
 
+    public  void clear(){
+        lines.clear();
+        invalidate();
+    }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        Log.i("ming","onTouch:"+event.getX()+"x"+event.getY()+"y");
-        return true;//true部會觸發onclick會持續觸發ontouch,false只會觸發一次ontouch而已
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            touchDown(event.getX(), event.getY());
+        }else if (event.getAction() == MotionEvent.ACTION_MOVE){
+            touchMove(event.getX(), event.getY());
+        }
+        return true; //super.onTouchEvent(event);
+    //true部會觸發onclick會持續觸發ontouch,false只會觸發一次ontouch而已
         // super.onTouchEvent(event)會觸發onclick並會持續觸發ontouch;
     }
 
+    private void touchDown(float x, float y){
+        LinkedList<Point> line = new LinkedList<>();
+        Point p = new Point();
+        p.x = x; p.y = y;
+        line.add(p);
+        lines.add(line);
+    }
+    private void touchMove(float x, float y){
+        LinkedList<Point> line = lines.getLast();
+        Point p = new Point();
+        p.x = x; p.y = y;
+        line.add(p);
+        invalidate();
+    }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Paint p = new Paint();
-        p.setColor(Color.BLUE);
-        p.setStrokeWidth(4);
-        canvas.drawLine(0,0,200,200,p);
+        for (LinkedList<Point> line: lines){
+            for (int i=1; i<line.size(); i++){
+                Point p0 = line.get(i-1);
+                Point p1 = line.get(i);
+                canvas.drawLine(p0.x,p0.y,p1.x,p1.y,paint);
+            }
+        }
 
+    }
+
+    public void setColor(int color){
+        paint.setColor(color);
+        invalidate();
+    }
+
+    public int getColor(){
+        return paint.getColor();
+    }
+    private class Point{
+        float x, y;
     }
 }
